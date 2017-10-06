e1283c4cf1e4813ae94579e60484048818291a38
Thierry Weissbeck <thierry.weissbeck@lyra-network.com>
2017-10-06 11:31:53 +0200

    Go to new activity, navigation

--- app/src/main/java/weather/lyra/com/weatherapp/MainActivity.kt
+++ app/src/main/java/weather/lyra/com/weatherapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package weather.lyra.com.weatherapp
 
+import android.content.Intent
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +10,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val waitTask = object : AsyncTask<Void, Void, Boolean>() {
+            override fun onPostExecute(result: Boolean?) {
+                goToWeatherActivity()
+            }
+
+
+            override fun doInBackground(vararg params: Void?): Boolean {
+                Thread.sleep(1000)
+                return true
+            }
+
+        }
+        waitTask.execute()
+    }
+
+    fun goToWeatherActivity() {
+        NavigationHelper.startActivity(this, WeatherActivity::class.java, clearStack = true)
     }
 }
