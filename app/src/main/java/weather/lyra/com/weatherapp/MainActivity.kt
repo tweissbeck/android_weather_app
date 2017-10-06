a42d488ff7ae1f94bd5d767fff2aec5ac8e39ca0
Thierry Weissbeck <thierry.weissbeck@lyra-network.com>
2017-10-06 12:04:24 +0200

    Mise a jour de l'ui sur l'async task

--- app/src/main/java/weather/lyra/com/weatherapp/MainActivity.kt
+++ app/src/main/java/weather/lyra/com/weatherapp/MainActivity.kt
@@ -4,12 +4,20 @@ import android.content.Intent
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
 
     }
@@ -17,17 +25,34 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        val waitTask = object : AsyncTask<Void, Void, Boolean>() {
+        val waitTask = object : AsyncTask<Void, Int, Boolean>() {
+
+
             override fun onPostExecute(result: Boolean?) {
+                loader.visibility = View.GONE
+                Thread.sleep(500)
                 goToWeatherActivity()
             }
 
+            override fun onPreExecute() {
+                super.onPreExecute()
+                loader.visibility = View.VISIBLE
+            }
 
             override fun doInBackground(vararg params: Void?): Boolean {
-                Thread.sleep(5000)
+
+                (1..5).map { Thread.sleep(it * 100L)
+                 publishProgress(it * 20)
+                }
+
+
                 return true
             }
 
+            override fun onProgressUpdate(vararg values: Int?) {
+               loader_progress_text.text = "${values[0]}/100"
+
+            }
         }
         waitTask.execute()
     }
